35f6410c81b83e4a77a80aa1a1ad3027667f6347
boudahiroki <sosya0904.h@gmail.com>
2020-08-20 15:24:20 +0900

    課題提出
    オーバーロード、継承、オーバーライド

---
+++ JavaStudy/sample/src/sample/Sum.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Sum {
+    public int plus(int a) {
+        return a + 1;
+    }
+    public int plus(int a, int b) {
+        return a + b;
+    }
+    public int plus(int a, int b, int c) {
+        return a + b + c;
+    }
+}
